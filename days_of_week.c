ba45f1bcc3ef26f22e6bffe53717bab0d05ad73a
Khushpreet Kaur <ky7079304@gmail.com>
2025-12-30 19:29:01 +0530

    Add files via upload

---
+++ days_of_week.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+int days;
+printf("enter day of week: ");
+scanf("%d",&days);
+if(days==1){
+    printf("sunday");
+}
+else if(days==2){
+    printf("monday");
+}
+else if(days==3){
+    printf("tuesday");
+}
+else if(days==4){
+    printf("wednesday");
+}
+else if(days==5){
+    printf("thursday");
+}
+else if(days==6){
+    printf("friday");
+}
+else if(days==7){
+    printf("saturday");
+}
+else{
+   printf("enter vaue between1-7");
+}
+return 0;
+}
